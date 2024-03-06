1e4cbffff590f00f169d5d22e9213c9f535a5401
guebebraga <guebe.braga5@gmail.com>
2024-03-05 23:04:47 -0300

    cambios en script

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,26 +1,23 @@
-const numB = document.getElementsByClassName("buscador")
-const botonBuscar = document.getElementsByClassName("boton")
-let lista = document.getElementsByClassName("lista")
+const buscarBalon=()=>{ 
 
-const numBalon = numB.value
+//const botonBuscar = document.getElementsByClassName("boton")
+const numB = document.getElementById("buscador")   
 
-const buscarBalon=()=>{    
-
- if(numBalon){
+ if(!numB.value){
+    let label = document.querySelector('.label');
+    label.textContent = "Debe ingresar un número de balón.";
+    label.style.color = "red";
+}
+else{
     fetch("mongodb://localhost:27017/airBackEnd/cilindro",{
-            method: "GET",
-            headers:{
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(numBalon),
-        })
-        .then(response => response.json())
-        .then(data => alert(data.mensaje));
-        
-    }
-    else{
-        let label = document.querySelector('.label');
-        label.textContent = "Debe ingresar un número de balón.";
-        label.style.color = "red";
-    }
+        method: "GET",
+        headers:{
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(numB),
+    })
+    .then(response => response.json())
+    .then(data => alert(data.mensaje));
+    console.log(numB)
+}
 };
