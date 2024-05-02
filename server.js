98f2d4ec32a91946441022f5de283a5284c32a78
guebebraga <guebe.braga5@gmail.com>
2024-05-02 20:37:55 -0300

    cambios en css

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ app.get('/', (req, res) => {
 
 // Iniciar el servidor
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Running in the port ${PORT}`);
 });
